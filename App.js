72d529071a0a077f9a2df2da262d9d8fbc7cc506
Yurback <ryv1983@mail.ru>
2021-04-30 18:25:37 +0300

    lovely tune

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = enteredText => setEnteredGoal(enteredText);
+
+  const addGoalHandler = () => setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen} >
+      <View style={styles.inputConteiner}>
+        <TextInput value={enteredGoal} placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <View>
+        <ScrollView>
+          {courseGoals.map((goal, index) => (
+            <View style={styles.listItem} key={index}>
+              <Text >{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputConteiner: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderWidth: 1,
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
+  }
 });
